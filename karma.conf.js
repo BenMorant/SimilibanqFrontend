308112f53b8a10f2735e13e5ba1279a04e06b48d
Benjamin Morant <bmorant@oxiane.com>
2021-01-19 12:07:42 +0100

    modified karma.conf.js  and added protractor-ci.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,12 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 module.exports = function (config) {
   config.set({
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
@@ -29,8 +33,9 @@ module.exports = function (config) {
     singleRun: false,
     restartOnFileChange: true,
     restartOnFileChange: true,
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
     customLaunchers: {
-      ChromeHeadlessCustom: {
+      ChromeHeadlessCI: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox', '--disable-gpu']
       }
